7a79fc218d183ed1d5a71b4a35d320fe4fce345c
Abdul Jamil Raoufi <abdul.raoufi@tekschool.us>
2022-11-09 21:45:23 -0500

    first commit in last try

---
+++ src/lastTry/LastTryClass.java
@@ -0,0 +1,13 @@
+package lastTry;
+
+public class LastTryClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("this is last try");
+		
+		
+	}
+
+}
